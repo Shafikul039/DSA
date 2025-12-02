c1efba99f70e0edff60c2b890c6b48e8a14f3217
Shafikul039 <148460717+Shafikul039@users.noreply.github.com>
2025-12-02 18:35:29 +0600

    Update FloorDivision.cpp

--- Math/FloorDivision.cpp
+++ Math/FloorDivision.cpp
@@ -1,4 +1,4 @@
-ll sum_floor_divisions(ll n) {
+ll floor_divisions(ll n) {
     ll sum = 0;
     for (ll left = 1, right; left <= n; left = right + 1) {
         ll k = n / left;
@@ -8,6 +8,19 @@ ll sum_floor_divisions(ll n) {
     }
     return sum;
 }
-
-// n/1 + n/2 + n/3 +....n/n
+// n/1 + n/2 + n/3 +....n/n 
 //O(sqrt(n))
+
+ll sum_floor_division(ll n) {
+    ll total = 0;
+    for (ll left = 1, right; left <= n; left = right + 1) {
+        ll k = n / left;
+        right = n / k;
+        ll count = (right - left + 1);
+        ll sum_i = (right + left) * count/2;
+        total = total + (sum_i * k);
+    }
+
+    return total;
+}
+// (n/1)*1 + (n/2)*2 + (n/3)*3 + .... (n/n)*n;
