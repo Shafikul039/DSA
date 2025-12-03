b691711f1a418dd0d44f4ce79bb17c6c70871238
Shafikul039 <148460717+Shafikul039@users.noreply.github.com>
2025-12-03 21:23:10 +0600

    Update SPF.cpp

--- Math/SPF.cpp
+++ Math/SPF.cpp
@@ -1,11 +1,13 @@
-const int N = 1e6 + 7;
+const int N = 1e6 + 2;
 int spf[N];
 
 void sieve() {
-    for (int i = 2; i < N; i++) {
-        if (spf[i] == 0) {
-            for (int j = i; j < N; j += i) {
-                if (spf[j] == 0) spf[j] = i;
+    for (int i = 2; i < N; i++) spf[i] = i;
+    for (int i = 2; i < N; i += 2) spf[i] = 2;
+    for (int i = 3; i*i < N; i += 2) {
+        if (spf[i] == i) {
+            for (int j = i*i; j < N; j += i) {
+                if (spf[j] == j) spf[j] = i;
             }
         }
     }
