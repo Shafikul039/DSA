770680a979827a8f396d002cd41d263d116d01eb
MD Shafikul Islam <148460717+Shafikul039@users.noreply.github.com>
2026-04-15 16:03:26 +0600

    Update Lis.cpp

--- Dynamic Programming/Lis.cpp
+++ Dynamic Programming/Lis.cpp
@@ -1,3 +1,4 @@
+//Finds The Size of The Lis
 ll Lis(vector<ll> &arr) {
     vector<ll> lis;
     for (auto &u : arr) {
@@ -8,4 +9,29 @@ ll Lis(vector<ll> &arr) {
     }
     
     return lis.size();
+}
+
+//Finds The Lis Array
+vector<ll> LisArray(vector<ll> &arr) {
+    ll n = arr.size();
+    vector<ll> lis, pos(n);
+    for (int i = 0; i < n; i++) {
+        auto it = lower_bound(lis.begin(), lis.end(), arr[i]);
+
+        pos[i] = it - lis.begin() + 1;
+        if (it == lis.end()) lis.push_back(arr[i]);
+        else *it = arr[i];
+    }
+    
+    vector<ll> ans;
+    ll curr = lis.size();
+    for (int i = n-1; i >= 0; i--) {
+        if (pos[i] == curr) {
+            ans.push_back(arr[i]);
+            curr--;
+        }
+    }
+    reverse(ans.begin(), ans.end());
+
+    return ans;
 }
